b307bf5b27893e048bac53425e0aa12beb8a2780
Lukáš Lešák <lukas.kalna@seznam.cz>
2023-02-05 11:19:31 +0100

    scroll pres js funkcni

--- Text na středu správně/script.js
+++ Text na středu správně/script.js
@@ -1,9 +1,25 @@
-(function($){
-    $(function(){
-        
+(function ($) {
+    $(function () {
+
         /* Scroll to sections */
-        $(".jq--about-towers-scroll").click(function(){
-           $("html, body").animate({scrollTop: $(".jq--about-towers").offset().top}, 1000);
+        $(".jq--about-towers-scroll").click(function () {
+            $("html, body").animate({ scrollTop: $(".jq--about-towers").offset().top }, 1000);
         });
-     });
- })(jQuery);
+
+        $(".jq--our-towers-scroll").click(function () {
+            $("html, body").animate({ scrollTop: $(".jq--our-towers").offset().top }, 1000);
+        });
+
+        $(".jq--references-scroll").click(function () {
+            $("html, body").animate({ scrollTop: $(".jq--references").offset().top }, 1000);
+        });
+
+        $(".jq--gellery-scroll").click(function () {
+            $("html, body").animate({ scrollTop: $(".jq--gellery").offset().top }, 1000);
+        });
+
+        $(".jq--form-scroll").click(function () {
+            $("html, body").animate({ scrollTop: $(".jq--form").offset().top }, 1000);
+        });
+    });
+})(jQuery);
